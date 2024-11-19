dbef40f7be969e94e6ad195ffedca2689e0a7693
Basile Pleplé <basile.pleple@hotmail.com>
2024-11-19 18:32:54 +0000

    column name

--- models/marts/mrt_listings_nbhd.sql
+++ models/marts/mrt_listings_nbhd.sql
@@ -5,7 +5,7 @@
 select
  neighbourhood_cleansed,
  room_type,
- count(id) as total_listings,
+ count(appt_id) as total_listings,
  avg(price) as avg_price,
 from {{ ref('stg_listings') }}
 group by 1,2
