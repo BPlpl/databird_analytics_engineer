bf8545656897b935519308c6a4f196491b7496a9
Basile Pleplé <basile.pleple@hotmail.com>
2024-11-25 19:09:58 +0000

    creating all needed mrt

---
+++ models/mart/mrt_local_bike__sales_per_category.sql
@@ -0,0 +1,19 @@
+
+SELECT  
+    s.store_name
+    ,FORMAT_DATE('%Y_%m', o.ordered_on) as order_month
+    ,c.category_name
+
+    ,sum(oi.quantity) as bikes_sold
+    ,avg(oi.quantity * p.list_unit_price_usd) as avg_order_amount_gross_discount
+    ,avg(oi.quantity * oi.list_unit_price_usd * oi.discount_pct) as avg_discount
+    ,avg(oi.quantity * oi.list_unit_price_usd * (1-oi.discount_pct)) as avg_order_amount_net_discount
+
+FROM 
+    {{ ref('stg_local_bike__products') }} p
+    LEFT JOIN {{ ref('stg_local_bike__categories') }} c on c.category_id = p.category_id
+    LEFT JOIN {{ ref('stg_local_bike__order_items') }} oi on oi.product_id = p.product_id
+    LEFT JOIN {{ ref('stg_local_bike__orders') }} o on o.order_id = oi.order_id
+    LEFT JOIN {{ ref('stg_local_bike__stores') }} s on s.store_id = o.store_id
+
+GROUP BY c.category_name, s.store_name, order_month
